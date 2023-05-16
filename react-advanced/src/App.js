276bcb26f8f286b7eb9a2c47e3287da67152f433
Parmesh Bhatt <101285245+Parmesh119@users.noreply.github.com>
2023-05-17 00:12:27 +0530

    Add files via upload

---
+++ react-advanced/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function App() {
+  return (
+    <div className='container'>
+      <h2>Parmesh Here is the Advanced Tutorial of React course</h2>
+    </div>
+  )
+}
+
+export default App
